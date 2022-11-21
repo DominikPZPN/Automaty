7cb6004130a76b5f184199fb6df0e50fc9f4019c
Dominik Kędzierski <dominik.kedzierski@hq.pzpn.pl>
2022-11-21 11:01:19 +0100

    first commit

---
+++ Keycloak.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace Automaty
+{
+    public class Keycloak
+    {
+        IWebDriver driver = new ChromeDriver();
+
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Logowanie()
+        {
+            KeycloakPage keycloakPage = new KeycloakPage(driver);
+
+            driver.Navigate().GoToUrl("https://logintest.laczynaspilka.pl/auth/realms/PZPN/protocol/openid-connect/auth?client_id=KLUBY_BACKEND&redirect_uri=https%3A%2F%2Fkluby-test.pzpn.pl");
+            keycloakPage.fillCredentials("sed_1@yopmail.com", "Haslo");
+
+        }
+
+        [Test]
+        public void Rejestracja()
+        {
+        }
+    }
+}
---
+++ KeycloakPage.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automaty
+{
+    public class KeycloakPage
+    {
+        IWebDriver driver;
+
+        public KeycloakPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public By userName = By.Id("username");
+        public By password = By.Id("password");
+
+        public void fillCredentials(string login, string passwordInput)
+        {
+            driver.FindElement(userName).SendKeys(login);
+            driver.FindElement(password).SendKeys(passwordInput);
+        }
+    }
+}
